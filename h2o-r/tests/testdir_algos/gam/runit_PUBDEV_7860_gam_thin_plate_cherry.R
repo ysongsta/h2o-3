3268d9b65678be7f0388bd233e373b39320289e1
wendycwong <wendycwong@yahoo.com>
2020-12-11 16:36:34 -0800

    Add 2 gam columns

--- h2o-r/tests/testdir_algos/gam/runit_PUBDEV_7860_gam_thin_plate_cherry.R
+++ h2o-r/tests/testdir_algos/gam/runit_PUBDEV_7860_gam_thin_plate_cherry.R
@@ -6,9 +6,12 @@ library(mgcv)
 # simple test to make sure we can invoke early stop in R for GAM.  Heavy testing is done in Python.  We
 # are just testing the client API.
 test.model.gam.thin.plates <- function() {
-    data(trees)
+    h2oTrain <- h2o.importFile("/Users/wendycwong/temp/Gaussian80Rows.csv")
+    rTrain <- as.data.frame(h2oTrain)
+   # data(trees)
     browser()
-    ct1 <- gam(Volume ~ s(Height, Girth, k = 6, sp = 0.1), family=Gamma(link=log), data=trees)
+    ct2 <- gam(C21 ~ s(C19, C20, k=7, sp = 0.1)+s(C17, C18, k=6, sp=0.1), family=Gamma(link=log), data=rTrain)
+  #  ct1 <- gam(Volume ~ s(Height, Girth, k = 6, sp = 0.1), family=Gamma(link=log), data=trees)
 }
 
 doTest("GAM with thin plate regression splines with cherry", test.model.gam.thin.plates)
